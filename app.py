9b7ea2b4611be3ffa696eb89f49f76fe99c339ed
Joshua Black <black.joshuad@gmail.com>
2021-01-30 12:00:58 +1300

    Added corpus to cooc networks, added cooc tables.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import text_content
 import cytoscape_helpers
 import text_helpers
+import cooc_table_helpers
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -28,50 +29,58 @@ app.layout = html.Div([
             children=text_helpers.text_tab
             ),
         dcc.Tab(
-            label='Cooccurence Networks',
-            children=cytoscape_helpers.cooc_tab),
+            label='Cooccurrence Networks',
+            children=cytoscape_helpers.cooc_tab
+            ),
         # dcc.Tab(
         #     label='Cooccurence Table',
         #     children=cytoscape_helpers.cooc_table)
+        dcc.Tab(
+            label='Cooccurrence Tables',
+            children=cooc_table_helpers.cooc_table_tab
+        )
     ])
 ])
-# Cooccurence callbacks.
-# First, change preloaded search terms given dictionary choice
+# Cooccurrence callbacks.
+# Change preloaded search terms given dictionary or corpus change choice
 @app.callback(
     Output(component_id='term', component_property='options'),
-    Input(component_id='dictionary', component_property='value')
+    Input(component_id='dictionary', component_property='value'),
+    Input(component_id='corpus-select', component_property='value')
 )
-def return_terms(dict_type):
+def return_terms(dict_type, corpus):
     """
     Given the type of dictionary required (string) for a
     collocation network, return the precalculated options (list).
     """
-    if dict_type == 'all':
-        words = cytoscape_helpers.ALL_WORD_TERMS
-    elif dict_type == 'propn':
-        words = cytoscape_helpers.PROPN_TERMS
-    elif dict_type == 'entities':
-        words = cytoscape_helpers.ENTITY_TERMS
+    words = cytoscape_helpers.preloaded_search_terms(dict_type, corpus)
+    formatted_words = [{'label': word, 'value': word} for word in words]
+    return formatted_words
 
-    return [{'label': word, 'value': word} for word in words]
 
+# Function on press submit, to change cooccurrence network.
 @app.callback(
-    Output(component_id='cooccurence-network', component_property='elements'),
-    Input(component_id='submit-val', component_property='n_clicks'),
+    [Output(component_id='cooccurrence-network', component_property='elements'),
+    Output(component_id='cooccurrence-network', component_property='stylesheet')],
+    [Input(component_id='submit-val', component_property='n_clicks')],
+    [State(component_id='corpus-select', component_property='value'),
     State(component_id='rep', component_property='value'),
     State(component_id='dictionary', component_property='value'),
     State(component_id='term', component_property='value'),
     State(component_id='stat-choice', component_property='value'),
     State(component_id='primary-coocs', component_property='value'),
-    State(component_id='secondary-coocs', component_property='value'),
+    State(component_id='secondary-coocs', component_property='value')]
 )
-def update_network(n_clicks, rep, dict, term, stat, pri_cooc_num, sec_cooc_num):
-    return cytoscape_helpers.generate_network(
-        rep, dict, term, stat, pri_cooc_num, sec_cooc_num
+def update_network(n_clicks, corpus, rep, dict, term, stat, pri_cooc_num, sec_cooc_num):
+    elements = cytoscape_helpers.generate_network(
+        corpus, rep, dict, term, stat, pri_cooc_num, sec_cooc_num
     )
+    style = cytoscape_helpers.change_cytoscape_width(stat)
+    return elements, style
 
 
-# Text display callbacks
+
+#### Text display callbacks
 # Update the displayed text.
 @app.callback(
     Output(component_id='article-markdown', component_property='children'),
@@ -83,11 +92,17 @@ def update_text_dangerous(index, search):
         boldface = None
     else:
         boldface = search
-    return text_helpers.text_as_markdown(
-        index,
-        text_helpers.TEXTS,
-        boldface=boldface
-    )
+
+    if index == '':
+        text = '### No matches found'
+    else:
+        text = text_helpers.text_as_markdown(
+            index,
+            text_helpers.TEXTS,
+            boldface=boldface
+        )
+
+    return text
 
 
 
@@ -97,8 +112,11 @@ def update_text_dangerous(index, search):
     Input(component_id='text-select', component_property='options')
 )
 def load_first_doc(indices):
-    return indices[0]['value']
-
+    if len(indices) > 1:
+        first_doc = indices[0]['value']
+    else:
+        first_doc = ''
+    return first_doc
 
 
 # Load subcorpus or filter by seach term.
@@ -110,7 +128,7 @@ def load_first_doc(indices):
 def change_corpus(subcorpus, search_term):
     ctx = dash.callback_context
     if not ctx.triggered:
-        component_id='sub_-orpus'
+        component_id='sub-corpus'
     else:
         component_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
@@ -136,6 +154,40 @@ def change_corpus(subcorpus, search_term):
 )
 def reset_searchbox(sub_corpus):
     return ''
+
+
+
+### Cooccurrence table updates.
+## Load new table on pressing 'submit'
+@app.callback(
+    Output(component_id='cooc-table', component_property='data'),
+    Input(component_id='table-submit-val', component_property='n_clicks'),
+    State(component_id='table-corpus-select', component_property='value'),
+    State(component_id='table-rep', component_property='value'),
+    State(component_id='table-dictionary', component_property='value'),
+    State(component_id='table-term', component_property='value'),
+    State(component_id='table-stat-choice', component_property='value')
+)
+def update_table(n_clicks, corpus, rep, dict, term, stat):
+    new_df = cooc_table_helpers.return_cooc_df(corpus, dict, term, rep, stat)
+    return new_df.to_dict('records')
+
+## Load new search terms on corpus change
+@app.callback(
+    Output(component_id='table-term', component_property='options'),
+    Input(component_id='table-dictionary', component_property='value'),
+    Input(component_id='table-corpus-select', component_property='value'),
+    Input(component_id='table-rep', component_property='value'),
+    Input(component_id='table-stat-choice', component_property='value')
+)
+def table_return_terms(dict, corpus, rep, stat):
+    """
+    Given the type of dictionary required (string) for a
+    collocation network, return the precalculated options (list).
+    """
+    words = cooc_table_helpers.load_search_terms(corpus, dict, rep, stat)
+    formatted_words = [{'label': word, 'value': word} for word in words]
+    return formatted_words
 
 
 if __name__ == '__main__':
--- text_content.py
+++ text_content.py
@@ -4,4 +4,6 @@ opening_text = """
 
 This app presents some initial experiments with the Papers Past Open Data
 release of early New Zealand newspaper content.
+
+Note: this page contains terms, and charts relationships between terms, from the text of nineteenth century newspapers using various statistical learning methods. As such, it is likely to contain offensive material.
 """
