cbeec9aee8082758f7267730a86c547df511140f
Joshua Black <black.joshuad@gmail.com>
2021-02-05 10:40:50 +1300

    More detailed text introduction.

--- text_content.py
+++ text_content.py
@@ -1,9 +1,16 @@
 opening_text = """
-# DATA601 Project
-## Using Data Science Methods to Investigate Philosophical Discourse in Early New Zealand Newspapers
+# Investigating Philosophical Discourse in Early New Zealand Newspapers
+## DATA601 Summer Project (UC Arts Digital Lab)
+Joshua Black
+black.joshuad@gmail.com
 
-This app presents some initial experiments with the Papers Past Open Data
-release of early New Zealand newspaper content.
+This dashboard allows for the inspection of a random subset of the main corpora developed in course of the project and for the generation and inspection of co-occurrence networks of key terms in the corpora.
 
-Note: this page contains terms, and charts relationships between terms, from the text of nineteenth century newspapers using various statistical learning methods. As such, it is likely to contain offensive material.
+The full general philosophy corpus and religion-science corpus are availble for download as zip files containing the articles in markdown format: (add links)
+
+The corpora, and labelled dataset used for training classifiers, are also available as pickled pandas dataframes along with almost all other project files and got at the project's [GitHub page](https://github.com/JoshuaDavidBlack/NPOD_Philosophy).
+
+Code for this dashboard is available at <https://github.com/JoshuaDavidBlack/NPOD_Philosophy>.
+
+**Note:** this page contains terms, and charts relationships between terms, from the text of nineteenth century newspapers using various statistical learning methods. As such, it is likely to contain offensive material.
 """
