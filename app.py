6e8705b752a7020dfa4f6e2d3de8bc730475fa02
Joshua Wilson Black <black.joshuad@gmail.com>
2022-10-10 10:06:53 +1300

    Add explicit server variable.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 dash_app.title = 'Philosophical Contestation in Early New Zealand Newspapers'
 
-app = dash_app.server
+server = dash_app.server
 
 dash_app.layout = dbc.Container([
     dcc.Markdown(children=text_content.opening_text),
